77e09733ee14b1416ca109f8cb22b9e4d1854842
Cassio Morales <cassioutfpr@gmail.com>
2022-08-03 21:46:02 -0300

    Add files via upload

---
+++ precache-manifest.529bf9e4ea761c4f8ecb05d3725806d8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be3cce0b74e204030907",
+    "url": "/css/app.3c89e39e.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "628d39722d80457a9f5d3e2303e64e21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef543153fbcd9553d3f3",
+    "url": "/js/about.d2e91dfd.js"
+  },
+  {
+    "revision": "be3cce0b74e204030907",
+    "url": "/js/app.0cb81122.js"
+  },
+  {
+    "revision": "c1f6a5576107aa57f489",
+    "url": "/js/chunk-vendors.1ff3816f.js"
+  },
+  {
+    "revision": "277b880d0964d13e0aac550d038bf4b6",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
